b0e4ecc2fb0ca419e5cb45c5e5dfd27c0749e9c4
febrianto99 <febrianmarkoto@gmail.com>
2019-07-17 13:46:21 +0700

    Push GitHUB Pertama

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/typography', function () {
+    return view('typography');
+});
+
+Route::get('/singlepost', function () {
+    return view('singlepost');
+});
+
+Route::get('/categories', function () {
+    return view('categories');
+});
+
+Route::get('dashboardfrontend', function () {
+    return view('dashboardfrontend');
+});
+	//admin
+		Route::group(
+			['prefix' => 'admin','middleware' =>['auth']],
+			function () {
+				Route::get('/', function () {
+					return view('backend');
+				});
+				Route::resource('kategori','KategoriController');
+				Route::resource('tag','TagController');
+				Route::resource('artikel','ArtikelController');
+			}
+		);
